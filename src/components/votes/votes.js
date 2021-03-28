2a1b6700c9d3579479a176f3235c1da3c4558269
kasireddysrikanth <kasireddysrikanth27@gmail.com>
2021-03-28 16:53:53 +0530

    alert added

--- src/components/votes/votes.js
+++ src/components/votes/votes.js
@@ -22,13 +22,13 @@ function Vote({ questionid, id, votes, unvotes }) {
     if (user && votes.find((vote) => vote.username === user.username)) {
       setVotecolor("red");
     } else setVotecolor("teal");
-  }, [user, votes]);
+  }, [user, votes,alert]);
 
   useEffect(() => {
     if (user && unvotes.find((unvote) => unvote.username === user.username)) {
       setUnvotecolor("red");
     } else setUnvotecolor("teal");
-  }, [user, unvotes]);
+  }, [user, unvotes,alert]);
 
 
   const submitVote = async () => {
