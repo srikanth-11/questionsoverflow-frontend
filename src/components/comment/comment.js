334bbfd21a3739e41626bbd672c42f7930451410
kasireddysrikanth <kasireddysrikanth27@gmail.com>
2021-05-07 18:47:55 +0530

    useContext imported

--- src/components/comment/comment.js
+++ src/components/comment/comment.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useContext } from 'react';
 import { username, token } from '../../context/usercontext'
 import { useAlert } from "react-alert";
 import {countContext} from '../../context/context'
--- src/components/singlequestion/singlequestion.js
+++ src/components/singlequestion/singlequestion.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useContext } from 'react';
 import { useHistory, Link } from "react-router-dom";
 
 import axios from 'axios'
